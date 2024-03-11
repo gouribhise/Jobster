59a1b0c38b342c460cd831f50eda17d79a2ad6e7
gouribhise <gouri.a.bhise@gmail.com>
2024-03-11 21:24:05 +0530

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
  
-
+import Landing from "./pages/Landing";
+import styled from "styled-components";
+ 
 function App() {
   return (
-  <h1>Jobster</h1>
+    <>
+       
+ <Landing/>
+    </>
+ 
   );
 }
 
