d8b31fa9fd59f1709053e0f26774fa4527021a8d
gouribhise <gouri.a.bhise@gmail.com>
2024-03-12 11:33:57 +0530

    added register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,39 @@
+import { useEffect,useState } from "react"
+import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/RegisterPage"
+import {Logo} from '../components'
+const initialState={
+    name:'',
+    email:'',
+    password:'',
+    isMember:true
+}
 const Register=()=>{
+    const[values,setValues]=useState(initialState)
+
+    const handleChange=(e)=>{
+        console.log(e.target)
+    }
+
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        console.log(e.target)
+    }
     return (
-        <div>register page</div>
+       <Wrapper className="full-page">
+        <form className="form" onSubmit={onSubmit}>
+            <Logo/>
+            <h3>Register</h3>
+            <div className="form-row">
+                <label htmlFor="name" className="form-label">name</label>
+                <input type='text' value={values.name} name='name' onChange={handleChange}
+                className="form-input"/>
+            </div>
+            <button type="submit" className="btn btn-block">Submit</button>
+
+        </form>
+
+       </Wrapper>
     )
 }
 
