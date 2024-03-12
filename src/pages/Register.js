ece92e2b1bf7ed8772e54235638fa541cf6f74e4
gouribhise <gouri.a.bhise@gmail.com>
2024-03-12 20:47:45 +0530

    completed ui of register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import { useEffect,useState } from "react"
 import { Link } from "react-router-dom"
 import Wrapper from "../assets/wrappers/RegisterPage"
-import {Logo} from '../components'
+import {Logo,FormRow} from '../components'
 const initialState={
     name:'',
     email:'',
@@ -24,11 +24,26 @@ const Register=()=>{
         <form className="form" onSubmit={onSubmit}>
             <Logo/>
             <h3>Register</h3>
-            <div className="form-row">
-                <label htmlFor="name" className="form-label">name</label>
-                <input type='text' value={values.name} name='name' onChange={handleChange}
-                className="form-input"/>
-            </div>
+            <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+            />
+
+<FormRow
+            type="email"
+            name="email"
+            value={values.email}
+            handleChange={handleChange}
+            />
+
+<FormRow
+            type="password"
+            name="password"
+            value={values.password}
+            handleChange={handleChange}
+            />
             <button type="submit" className="btn btn-block">Submit</button>
 
         </form>
