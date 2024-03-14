263bcc2c956d84cce8cbe729ddb4f45546d159a5
gouribhise <gouri.a.bhise@gmail.com>
2024-03-14 10:40:32 +0530

    shownhide smallsidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,13 +1,31 @@
-import Wrapper from "../assets/wrappers/SmallSidebar"
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
+export const SmallSidebar = () => {
+    const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
 
-const SmallSidebar=()=>{
-    return (
-        <>
-        <Wrapper>
-            <h2>SmallSidebar</h2>
-        </Wrapper>
-        </>
-    )
-}
+  return (
+    <Wrapper>
+    <div className={isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+    <div className='content'>
+        <button type='button' className='close-btn' onClick={toggle}>
+          <FaTimes />
+        </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default SmallSidebar
+export default SmallSidebar;
