24abc87605473c86c59e59e362f301ac31b20da3
gouribhise <gouri.a.bhise@gmail.com>
2024-03-14 09:33:49 +0530

    Added sharedlayout to set dashboard

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import {Landing,Error,Dashboard,Register} from "./pages";
+import {Landing,Error,Register} from "./pages";
+import {
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+  AddJob,
+} from './pages/dashboard';
   
 function App() {
   return (
     <>
        <BrowserRouter>
        <Routes>
-        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/" element={<SharedLayout/>}>
+          <Route index element={<Stats/>}/>
+          <Route path="all-jobs" element={<AllJobs/>}/>
+          <Route path="add-job" element={<AddJob/>}/>
+          <Route path="profile" element={<Profile/>}/>
+          </Route>
         <Route path="landing" element={<Landing/>}/>
         <Route path="register" element={<Register/>}/>
         <Route path="*" element={<Error/>}/>
